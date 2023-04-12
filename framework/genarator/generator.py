9f3b59be354562c8c9509e61ebff426de3f8c245
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-12 17:24:18 +0300

    added generation of new person data

--- framework/genarator/generator.py
+++ framework/genarator/generator.py
@@ -1,3 +1,4 @@
+from random import randint
 from ..data import Person
 from faker import Faker
 
@@ -8,6 +9,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + ' ' + faker_ru.last_name() + ' ' + faker_ru.middle_name(),
+        first_name=faker_ru.first_name(),
+        last_name=faker_ru.last_name(),
+        age=randint(10, 80),
+        salary=randint(10000, 100000),
+        department=faker_ru.job(),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
