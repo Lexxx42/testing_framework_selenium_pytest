adb5f40fe80792cd5240064ab7858dc358c1f9ca
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-12 17:19:45 +0300

    added new person data for form fields

--- framework/data/data.py
+++ framework/data/data.py
@@ -4,6 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class Person():
     full_name: str = ""
+    first_name: str = ""
+    last_name: str = ""
+    age: int = -1
+    salary: int = -1
+    department: str = ""
     email: str = ""
     current_address: str = ""
     permanent_address: str = ""
