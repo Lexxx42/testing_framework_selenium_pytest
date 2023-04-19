eaaf7c334024abbbde05df6a1a228a17a172e48b
Alexandr Konukhov <jesusmisha@gmail.com>
2023-04-19 11:45:52 +0300

    added date of birth locators

--- framework/locators/form_page_locators.py
+++ framework/locators/form_page_locators.py
@@ -9,15 +9,23 @@ class PracticeFormLocators:
     EMAIL_INPUT = (By.CSS_SELECTOR, '#userEmail')
     GENDER_RADIOBUTTON = (By.CSS_SELECTOR, f'[for=\'gender-radio-{randint(1, 3)}\']')
     MOBILE_INPUT = (By.CSS_SELECTOR, '#userNumber')
-    DATE_OF_BIRTH_INPUT = (By.CSS_SELECTOR, '#dateOfBirthInput')
+
+    # date of birth calendar
+    DATE_OF_BIRTH_SELECT = (By.CSS_SELECTOR, '#dateOfBirthInput')
+    DATE_OF_BIRTH_MONTH_DROPDOWN = (By.CSS_SELECTOR, '.react-datepicker__month-select')
+    DATE_OF_BIRTH_YEAR_DROPDOWN = (By.CSS_SELECTOR, '.react-datepicker__year-select')
+    DATE_OF_BIRTH_DAY_LIST = (By.CSS_SELECTOR, '[role=\'option\']')
+
     SUBJECTS = (By.CSS_SELECTOR, '#subjectsInput')
     HOBBIES = (By.CSS_SELECTOR, f'[for=\'hobbies-checkbox-{randint(1, 3)}\']')
     FILE_INPUT = (By.CSS_SELECTOR, '#uploadPicture')
     CURRENT_ADDRESS_INPUT = (By.CSS_SELECTOR, '#currentAddress')
+
     STATE_SELECT = (By.CSS_SELECTOR, '#state')
     STATE_INPUT = (By.CSS_SELECTOR, '#react-select-3-input')
     CITY_SELECT = (By.CSS_SELECTOR, '#city')
     CITY_INPUT = (By.CSS_SELECTOR, '#react-select-4-input')
+
     SUBMIT_BUTTON = (By.CSS_SELECTOR, '#submit')
 
     # table results
