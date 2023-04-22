bc4cb2a6b3176813ccd3cf67e62d1fc2e3bdaefe
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-22 23:14:39 +0300

    added alert page tests

--- framework/tests/alerts_frame_windows_test.py
+++ framework/tests/alerts_frame_windows_test.py
@@ -1,4 +1,5 @@
-from ..pages import BrowserWindowsPage
+from random import randint
+from ..pages import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindows:
@@ -18,3 +19,35 @@ class TestAlertsFrameWindows:
             new_window_text = browser_windows_page.check_opened('window')
             assert new_window_text == 'This is a sample page', \
                 f'New window text should be \'This is a sample page\' but got {new_window_text}'
+
+    class TestAlerts:
+        alerts_page_link = 'https://demoqa.com/alerts'
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, self.alerts_page_link)
+            alert_page.open()
+            alert_text = alert_page.check_see_alert('alert')
+            assert alert_text == 'You clicked a button', \
+                f'Alert text should be \'You clicked a button\'. Got {alert_text}'
+
+        def test_see_alert_after_five_sev(self, driver):
+            alert_page = AlertsPage(driver, self.alerts_page_link)
+            alert_page.open()
+            alert_text = alert_page.check_see_alert('alert_after_five_sec')
+            assert alert_text == 'This alert appeared after 5 seconds', \
+                f'Alert text should be \'This alert appeared after 5 seconds\'.' \
+                f' Got {alert_text}'
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, self.alerts_page_link)
+            alert_page.open()
+            options = ['Ok', 'Cancel']
+            selected_option = options[randint(-1, 0)]
+            alert_text = alert_page.check_see_alert('confirm', option=selected_option)
+            assert alert_text == 'Do you confirm action?', \
+                f'Alert text should be \'Do you confirm action?\'.' \
+                f' Got {alert_text}'
+            confirm_result = alert_page.check_confirm_result()
+            assert confirm_result == f'You selected {selected_option}', \
+                f'Confirm text should be \'You selected (Ok or Cancel)\'. ' \
+                f'Got: You selected {selected_option}'
