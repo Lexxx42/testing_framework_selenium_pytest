0a0e26f740817bb478e388db0d50685311b06af3
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-23 19:34:46 +0300

    added imports for modal dialogs

--- framework/pages/__init__.py
+++ framework/pages/__init__.py
@@ -1,4 +1,5 @@
 from .elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, LinksPage, \
     BrokenLinksPage, UploadAndDownloadPage, DynamicPropertiesPage
 from .form_page import PracticeFormPage
-from .alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage, NestedFramesPage
+from .alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage, NestedFramesPage, \
+    ModalDialogsPage
