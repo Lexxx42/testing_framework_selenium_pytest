98972b98f444548062f7ac84fc70613f7dba76ff
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-23 17:33:57 +0300

    added imports for frames tests

--- framework/pages/__init__.py
+++ framework/pages/__init__.py
@@ -1,4 +1,4 @@
 from .elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, LinksPage, \
     BrokenLinksPage, UploadAndDownloadPage, DynamicPropertiesPage
 from .form_page import PracticeFormPage
-from .alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
+from .alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage
