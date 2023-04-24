bd859816d97dbe8ce43e775b0486aebe8f28a416
Alexandr Konukhov <jesusmisha@gmail.com>
2023-04-24 11:49:02 +0300

    added docstrings

--- framework/pages/elements_page.py
+++ framework/pages/elements_page.py
@@ -1,3 +1,16 @@
+"""Module contains tests for Elements tab on the site.
+
+Contains page objects for:
+Text Box,
+Check Box,
+Radio Button,
+Web Tables,
+Buttons,
+Links,
+Broken Links - Images,
+Upload and Download,
+Dynamic Properties.
+"""
 import base64
 import time
 from os import remove, path
@@ -13,9 +26,11 @@ from ..generator import generated_person, generated_file
 
 
 class TextBoxPage(BasePage):
+    """Text Box page object."""
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
+        """Fill all form fields with generated data."""
         person_info = next(generated_person())
         full_name = person_info.full_name
         email = person_info.email
@@ -29,6 +44,7 @@ class TextBoxPage(BasePage):
         return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
+        """Check output data of sent form."""
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[-1]
         email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[-1]
         current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[-1]
@@ -37,12 +53,15 @@ class TextBoxPage(BasePage):
 
 
 class CheckBoxPage(BasePage):
+    """Check Box page object."""
     locators = CheckBoxPageLocators()
 
     def open_fill_list(self):
+        """Open dropdown list."""
         self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
 
     def click_random_checkbox(self):
+        """Activates random checkboxes."""
         item_list = self.elements_are_visible(self.locators.ITEM_LIST)
         count = 21
         while count > 0:
@@ -52,32 +71,39 @@ class CheckBoxPage(BasePage):
             count -= 1
 
     def get_checked_checkboxes(self):
+        """Returns activated checkboxes text."""
         checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)
         data = [checkbox.find_element('xpath', self.locators.TITLE_CHECKBOX).text for checkbox in checked_list]
         return str(data).replace(' ', '').replace('.doc', '').lower()
 
     def get_output_result(self):
+        """Returns output result of active checkboxes."""
         result_list = self.elements_are_present(self.locators.OUTPUT_RESULT_LIST)
         data = [item.text for item in result_list]
         return str(data).replace(' ', '').lower()
 
 
 class RadioButtonPage(BasePage):
+    """Radio Button page object."""
     locators = RadioButtonPageLocators()
 
     def click_on_the_radio_button(self, choice):
+        """Click on radio button."""
         choices = {'yes': self.locators.RADIO_BUTTON_YES, 'impressive': self.locators.RADIO_BUTTON_IMPRESSIVE,
                    'no': self.locators.RADIO_BUTTON_NO}
         self.element_is_visible(choices[choice]).click()
 
     def get_output_result(self):
+        """Returns output result of active radio button."""
         return self.element_is_present(self.locators.OUTPUT_RESULT).text
 
 
 class WebTablePage(BasePage):
+    """Web Tables page object."""
     locators = WebTablePageLocators()
 
     def add_new_person(self, count=1):
+        """Adds count numbers of persons with generated data to the table."""
         while count > 0:
             person_info = next(generated_person())
             first_name = person_info.first_name
@@ -98,19 +124,23 @@ class WebTablePage(BasePage):
         return [first_name, last_name, str(age), email, str(salary), department]
 
     def check_added_person(self):
+        """Returns list of persons from the table."""
         people_list = self.elements_are_present(self.locators.FULL_PEOPLE_LIST)
         data = [item.text.splitlines() for item in people_list]
         return data
 
     def search_some_person(self, key_word):
+        """Searches for a person with a key_word in a table."""
         self.element_is_visible(self.locators.SEARCH_FIELD).send_keys(key_word)
 
     def check_search_person(self):
+        """Check search result."""
         delete_button = self.element_is_present(self.locators.DELETE_BUTTON)
         row = delete_button.find_element('xpath', self.locators.ROW_PARENT)
         return row.text.splitlines()
 
     def update_person_info(self, field_to_edit='age'):
+        """Update person info in the table."""
         person_info = next(generated_person())
         edited_data = getattr(person_info, field_to_edit)
         self.element_is_visible(self.locators.UPDATE_BUTTON).click()
@@ -121,12 +151,15 @@ class WebTablePage(BasePage):
         return str(edited_data)
 
     def delete_person(self):
+        """Delete person info from the table."""
         self.element_is_visible(self.locators.DELETE_BUTTON).click()
 
     def check_delete(self):
+        """Check if person info deleted from the table."""
         return self.element_is_present(self.locators.NO_ROWS_FOUND).text
 
     def select_numer_of_rows(self):
+        """Change table row count appearance."""
         number_of_rows = [5, 10, 20, 25, 50, 100]
         data = []
         for number in number_of_rows:
@@ -138,36 +171,46 @@ class WebTablePage(BasePage):
         return data
 
     def check_count_of_rows(self):
+        """Check row count appearance."""
         return len(self.elements_are_present(self.locators.FULL_PEOPLE_LIST))
 
 
 class ButtonsPage(BasePage):
+    """Buttons page object."""
     locators = ButtonsPageLocators()
 
     def perform_double_click(self, locator=None):
+        """Performs double-click on element."""
         super().perform_double_click(self.locators.DOUBLE_CLICK_ME_BUTTON)
 
     def perform_right_click(self, locator=None):
+        """Performs right-click on element."""
         super().perform_right_click(self.locators.RIGHT_CLICK_ME_BUTTON)
 
     def perform_dynamic_click(self, locator=None):
+        """Performs dynamic-click on element."""
         super().perform_dynamic_click(self.locators.CLICK_ME_BUTTON)
 
     def check_dynamic_click_message(self):
+        """Check dynamic-click result message."""
         return self.element_is_present(self.locators.CLICK_MESSAGE).text
 
     def check_double_click_message(self):
+        """Check double-click result message."""
         return self.element_is_present(self.locators.DOUBLE_CLICK_MESSAGE).text
 
     def check_right_click_message(self):
+        """Check right-click result message."""
         return self.element_is_present(self.locators.RIGHT_CLICK_MESSAGE).text
 
 
 class LinksPage(BasePage):
+    """Links page object."""
     locators = LinksPageLocators()
     NO_ERRORS = 'No errors'
 
     def check_new_tab_simple_link(self):
+        """Check if new tab is opened."""
         simple_link = self.element_is_visible(self.locators.SIMPLE_LINK)
         link_href = simple_link.get_attribute('href')
         try:
@@ -179,6 +222,7 @@ class LinksPage(BasePage):
         return link_href, url
 
     def check_broken_link(self, url):
+        """Check broken link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.BAD_REQUEST).click()
@@ -187,6 +231,7 @@ class LinksPage(BasePage):
         return request.status_code, self.NO_ERRORS
 
     def check_new_tab_dynamic_link(self):
+        """Check if new tab is opened on dynamic link."""
         simple_link = self.element_is_visible(self.locators.DYNAMIC_LINK)
         link_href = simple_link.get_attribute('href')
         try:
@@ -198,6 +243,7 @@ class LinksPage(BasePage):
         return link_href, url
 
     def check_created_link(self, url):
+        """Check created link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.CREATED_REQUEST).click()
@@ -206,6 +252,7 @@ class LinksPage(BasePage):
         return request.status_code, self.NO_ERRORS
 
     def check_no_content_link(self, url):
+        """Check no content link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.NO_CONTENT_REQUEST).click()
@@ -214,6 +261,7 @@ class LinksPage(BasePage):
         return request.status_code, self.NO_ERRORS
 
     def check_moved_link(self, url):
+        """Check moved link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.MOVED_REQUEST).click()
@@ -222,6 +270,7 @@ class LinksPage(BasePage):
         return request.status_code, self.NO_ERRORS
 
     def check_unauthorized_link(self, url):
+        """Check unauthorized link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.UNAUTHORIZED_REQUEST).click()
@@ -230,6 +279,7 @@ class LinksPage(BasePage):
         return request.status_code, self.NO_ERRORS
 
     def check_forbidden_link(self, url):
+        """Check forbidden link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.FORBIDDEN_REQUEST).click()
@@ -238,6 +288,7 @@ class LinksPage(BasePage):
         return request.status_code, self.NO_ERRORS
 
     def check_not_found_link(self, url):
+        """Check not found link status code."""
         try:
             request = requests.get(url)
             self.element_is_present(self.locators.NOT_FOUND_REQUEST).click()
@@ -247,10 +298,12 @@ class LinksPage(BasePage):
 
 
 class BrokenLinksPage(BasePage):
+    """Broken Links page object."""
     locators = BrokenLinksPageLocators()
     NO_ERRORS = 'No errors'
 
     def check_valid_image_for_200_response(self):
+        """Check valid image status code and content type."""
         image = self.element_is_visible(self.locators.VALID_IMAGE)
         try:
             image_link = image.get_attribute('src')
@@ -261,6 +314,7 @@ class BrokenLinksPage(BasePage):
         return request.status_code, content_type, self.NO_ERRORS
 
     def check_broken_image_for_200_response(self):
+        """Check broken image status code and content type."""
         image = self.element_is_visible(self.locators.BROKEN_IMAGE)
         image_link = image.get_attribute('src')
         try:
@@ -271,6 +325,7 @@ class BrokenLinksPage(BasePage):
         return request.status_code, content_type, self.NO_ERRORS
 
     def check_valid_link(self):
+        """Check valid link href and url."""
         valid_link = self.element_is_visible(self.locators.VALID_LINK)
         link_href = valid_link.get_attribute('href')
         try:
@@ -282,6 +337,7 @@ class BrokenLinksPage(BasePage):
         return link_href.split('/')[2], url.split('/')[2]
 
     def check_broken_link(self):
+        """Check broken link status code and url."""
         broken_link = self.element_is_visible(self.locators.BROKEN_LINK)
         link_href = broken_link.get_attribute('href')
         try:
@@ -294,9 +350,11 @@ class BrokenLinksPage(BasePage):
 
 
 class UploadAndDownloadPage(BasePage):
+    """Upload and Download page object."""
     locators = UploadAndDownloadPageLocators()
 
     def upload_file(self):
+        """Uploads file. Returns filename and upload text result."""
         file_name, path = generated_file()
         try:
             self.element_is_visible(self.locators.UPLOAD_FILE_BUTTON).send_keys(path)
@@ -307,6 +365,7 @@ class UploadAndDownloadPage(BasePage):
         return file_name, upload_file_text.split('\\')[-1]
 
     def download_file(self):
+        """Downloads file. Returns generated file."""
         try:
             link = self.element_is_visible(self.locators.DOWNLOAD_FILE_BUTTON).get_attribute('href')
             link_bytes = base64.b64decode(link)
@@ -323,9 +382,11 @@ class UploadAndDownloadPage(BasePage):
 
 
 class DynamicPropertiesPage(BasePage):
+    """Dynamic Properties page object."""
     locators = DynamicPropertiesPageLocators()
 
     def check_enabled_button(self):
+        """Returns if button was enabled or not."""
         try:
             self.element_is_clickable(self.locators.ENABLE_AFTER_FIVE_SEC_BUTTON)
         except TimeoutException:
@@ -333,6 +394,7 @@ class DynamicPropertiesPage(BasePage):
         return True
 
     def check_change_of_color(self):
+        """Checks change of color of the button."""
         color_button = self.element_is_visible(self.locators.COLOR_CHANGE_BUTTON)
         color_before = color_button.value_of_css_property('color')
         time.sleep(5)
@@ -340,6 +402,7 @@ class DynamicPropertiesPage(BasePage):
         return color_before, color_after
 
     def check_button_appearance(self):
+        """Checks button appearance."""
         try:
             self.element_is_visible(self.locators.VISIBLE_AFTER_FIVE_SEC_BUTTON, 1)
         except TimeoutException:
