4dd9f14b0f0f737c2a39f11f6f09a66fce2df9da
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-04 11:51:02 +0300

    refactored test_created_link to have expected result as variable

--- framework/tests/elements_test.py
+++ framework/tests/elements_test.py
@@ -225,9 +225,10 @@ class TestElements:
             links_page.open()
             response_code, error_message = \
                 links_page.check_created_link(self.links_page_created_link)
-            assert response_code == 201, \
+            expected_status_code = 201
+            assert response_code == expected_status_code, \
                 f'Status code from {self.links_page_created_link}' \
-                f' should be 201 but got {response_code}' \
+                f' should be {expected_status_code} but got {response_code}' \
                 f'\nError: {error_message}'
 
         def test_no_content_link(self, driver):
