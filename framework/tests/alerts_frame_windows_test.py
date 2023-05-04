d0ef77e0992c4619388c1dd2b1c87cdde1df838d
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-04 12:02:42 +0300

    refactored test_large_modal_text to have expected result as variable

--- framework/tests/alerts_frame_windows_test.py
+++ framework/tests/alerts_frame_windows_test.py
@@ -231,19 +231,20 @@ class TestAlertsFrameWindows:
             modal_dialog_page.open()
             modal_dialog_page.open_modal('large')
             modal_text = modal_dialog_page.get_modal_text('large')
-            assert modal_text == 'Lorem Ipsum is simply dummy text ' \
-                                 'of the printing and typesetting industry.' \
-                                 ' Lorem Ipsum has been the industry\'s standard' \
-                                 ' dummy text ever since the 1500s, when an unknown' \
-                                 ' printer took a galley of type and scrambled' \
-                                 ' it to make a type specimen book. ' \
-                                 'It has survived not only five centuries, ' \
-                                 'but also the leap into electronic typesetting, ' \
-                                 'remaining essentially unchanged. ' \
-                                 'It was popularised in the 1960s with the release of ' \
-                                 'Letraset sheets containing Lorem Ipsum passages, ' \
-                                 'and more recently with desktop publishing software ' \
-                                 'like Aldus PageMaker including versions of Lorem Ipsum.', \
+            expected_modal_text = 'Lorem Ipsum is simply dummy text ' \
+                                  'of the printing and typesetting industry.' \
+                                  ' Lorem Ipsum has been the industry\'s standard' \
+                                  ' dummy text ever since the 1500s, when an unknown' \
+                                  ' printer took a galley of type and scrambled' \
+                                  ' it to make a type specimen book. ' \
+                                  'It has survived not only five centuries, ' \
+                                  'but also the leap into electronic typesetting, ' \
+                                  'remaining essentially unchanged. ' \
+                                  'It was popularised in the 1960s with the release of ' \
+                                  'Letraset sheets containing Lorem Ipsum passages, ' \
+                                  'and more recently with desktop publishing software ' \
+                                  'like Aldus PageMaker including versions of Lorem Ipsum.'
+            assert modal_text == expected_modal_text, \
                 'Text in modal dialog don\'t match expected text. ' \
                 f'Got {modal_text}'
 
