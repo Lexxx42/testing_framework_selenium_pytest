02d4dbf7cee48ad7c9f7ff6784c7ee905c6ba15c
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-06 09:59:36 +0300

    added imports

--- framework/locators/__init__.py
+++ framework/locators/__init__.py
@@ -7,4 +7,4 @@ from .form_page_locators import PracticeFormLocators
 from .alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators, \
     FramesPageLocators, NestedFramesPageLocators, ModalDialogsPageLocators
 from .widgets_page_locators import AccordianPageLocators, AutoCompletePageLocators, \
-    DatePickerPageLocators, SliderPageLocators
+    DatePickerPageLocators, SliderPageLocators, ProgressBarPageLocators
